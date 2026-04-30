be79b276a58d523b6a675c84d00059bfcd9d4f94
blogsteller-rgb <blogsteller@gmail.com>
2026-05-01 05:30:22 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const GITHUB_CONFIG = {
   owner: 'blogsteller-rgb',
   repo: 'family',
-  path: 'data/pets.json',
+  path: 'data/pets_data.json',
   token: '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'
 };
